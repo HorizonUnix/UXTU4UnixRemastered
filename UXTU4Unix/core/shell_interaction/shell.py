93d153b78926b2e77321d81c15d1389073434b77
Notch Apple <96767159+NotchApple1703@users.noreply.github.com>
2024-05-14 15:55:50 +0700

    Add files via upload

--- UXTU4Unix/core/shell_interaction/shell.py
+++ UXTU4Unix/core/shell_interaction/shell.py
@@ -1,6 +1,6 @@
 import subprocess
 
-from subprocess import PIPE
+from subprocess import PIPE, DEVNULL
 
 
 class Shell:
@@ -11,12 +11,12 @@ class Shell:
             raise Exception("Incorrect Password")
 
     def run(self, command: str) -> str:
-        with subprocess.Popen(f"sudo -S {command}", shell=True, stdin=PIPE, stdout=PIPE) as process:
+        with subprocess.Popen(f"sudo -S {command}", shell=True, stdin=PIPE, stdout=PIPE, stderr=DEVNULL) as process:
             output, _ = process.communicate(input=self.password)
             return output.decode()
 
     def password_is_correct(self) -> bool:
-        with subprocess.Popen("sudo -S echo", shell=True, stdin=PIPE) as process:
+        with subprocess.Popen("sudo -S echo", shell=True, stdin=PIPE, stderr=DEVNULL) as process:
             process.communicate(input=self.password)
 
             return not process.returncode
